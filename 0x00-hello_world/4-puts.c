02d40ca4f0671be86ea8ceea238e883e0e99090b
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2023-12-15 17:51:54 +0000

    Hello, puts-1-2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
-
 #include <stdio.h>
 
 /**
-* main - Entry point
-*
-* Return 0 if succesfully the program executed
-*/
+ * main - Entry point
+ *
+ * puts is function c that print string directly to the standard output
+ * Return 0 if succesfully the program executed
+ */
+
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
-	
-	return (0); /* will return 0 if the program runs good*/
+	return (0);
 }
