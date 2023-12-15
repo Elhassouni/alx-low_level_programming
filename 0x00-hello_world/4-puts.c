b08633d3d1af65387cdf8e11f3ddde6e954cf6c8
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2023-12-15 17:35:45 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 
 #include <stdio.h>
 
+
+/**
+* main - Entry point
+*
+* Return 0 if succesfully the program executed
+*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
