fd95b319d7a31acd561a7bd150d75496683d3902
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2023-12-16 04:03:15 +0000

    The last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program to print positive and negative number
+ *
+ * Description: Print whether the number stored in n is positive or negative
+ *
+ * Return: alawys 0 (sucess)
+ */
+
+int main(void)
+{
+	/* I used modelo operator to get the last digit*/
+	int n;
+	int lastnumber;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastnumber = n % 10;
+	if (lastnumber > 5)
+	{
+	printf("Last digit of %d is %d is greater than 5", n, lastnumber);
+	}
+	else if (lastnumber == 0)
+	{
+	printf("Last digit of %d is %d and is 0", n, lastnumber);
+	}
+	else if (lastnumber < 6 && lastnumber != 0)
+	{
+	printf("Last digit of %d is %d  and is less than 6 and not 0", n, lastnumber);
+	}
+	return (0); /*Return 0 for success*/
+}
