2201dfc5d7c6b2bc09217e4d98aa3a4db7c53d79
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2023-12-20 16:38:24 +0000

    warning: missing initial short description

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+  * print_alphabet - Printing alphabet in lowercase.
+  *
+  * Description: using while loop with local variable.
+  */
 void print_alphabet(void)
 {
 	int i = 97;
