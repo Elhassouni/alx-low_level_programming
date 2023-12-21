f723c381b5085969a24f7ca9023886064b198ce9
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2023-12-21 21:25:14 +0000

    Debug Multiple mains

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * print_sign - function that checks for alphabetic character.
+  *
+  * @n: The character to check.
+  *
+  *  Description: This function uses an if statement to return 1 and + sign
+  *  if positive and 0 if below zero and negative sign otherwise
+  *
+  * Return:  0 for succes and 1  otherwise.
+  */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+	
+	return (0);
+}
