486fc367e6374f624dd87bef7cc904959755fda5
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2023-12-27 18:17:03 +0000

    Table-times -base-10

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,51 +1,52 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-  * times_table - function that prints the 9 times table, starting with 0
-  *
-  * Description: This function is using putchar function to print numbers.
-  *
-  * Return: no return type.
-  */
-
+ * times_table - function that prints the 9 times table, starting with 0
+ *
+ * Description: This function uses _putchar to print numbers.
+ *
+ * Return: no return type.
+ */
 void times_table(void)
 {
-
-	int i;
+	int i, j, number;
 
 	i = 0;
 	while (i < 10)
 	{
-		int j;
-
 		j = 0;
 		while (j < 10)
 		{
-			int number = i * j;
+			number = i * j;
+
+			if (j != 0)
+			{
+				if (number <= 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
 
 			if (number >= 10)
 			{
-				_putchar('0' + number / 10);
+			_putchar('0' + number / 10);/*print first place*/
 			}
-			_putchar('0' + number % 10);  /* print ones place */
+			_putchar('0' + number % 10);  /* print second place */
+
 			if (j == 9)
 			{
 				_putchar('\n');
-			}
-			else if (number < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
 			}
 			j++;
 		}
 		i++;
 	}
 }
+
