4b7b11a7f3581f029babdedad2fdb4bdb5537d66
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2023-12-29 20:00:26 +0100

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 
 /**
- * add - Adds two numbers and returns the result.
+ * print_to_98 - Prints numbers from n to 98.
  *
- * @firstnumber: The first number to be added.
- * @secondnumber: The second number to be added.
+ * @n: The starting number.
  *
- * Description: This function takes two parameters, performs the addition
- * operation, and returns the result.
+ * Description: This function prints numbers from the given starting
+ * number (@n) to 98, separated by commas. It handles different cases
+ * based on the value of @n.
  *
- * Return: The sum of @firstnumber and @secondnumber.
+ * Return: No return value.
  */
 
 void print_to_98(int n)
@@ -24,43 +24,23 @@ void print_to_98(int n)
 		if (n <= i)
 		{
 			printf("%d", n);
-			if (n == 98)
-			{
-				printf("\n");
-			}
-			else
-			{
-				printf(", ");
-			}
 			n++;
 		}
 		else if (n > 98)
 		{
 			printf("%d", n);
-		 	
-			if (n == 98)
-			{
-				printf("\n");
- 			}
-			else
-			{
-				printf(", ");
-			}
 			n--;
 			}
 		else if (n < 0)
 		{
-			printf( "%d", n);
-			if (n ==98)
-			{
-				printf("\n");
-			}
-			else
-			{
-				printf(", ");
-			}
+			printf("%d", n);
 			n--;
 		}
+		if (n == 99)
+			printf("\n");
+		else
+			printf(", ");
 	}
 	printf("\n");
+
 }
