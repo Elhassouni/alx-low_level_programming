082f22c60d3a5ab2df09ba53232ebe7bb7cbe565
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-01-02 14:59:23 +0100

    200~0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <ctype.h>
 
 /**
   * _isupper -  function that checks for uppercase character.
@@ -12,7 +11,7 @@
 
 int _isupper(int c)
 {
-	if (isupper(c))
+	if (c >= 65 && c <= 90)
 
 		return (1);
 	else
