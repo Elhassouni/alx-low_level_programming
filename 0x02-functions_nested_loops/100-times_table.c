6daf15aa17631fa60c48481a529494907ec9b74b
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-01-02 01:51:54 +0100

    12. The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,7 +4,7 @@
  * print_times_table - Prints the times table up to a given number.
  * @n: The number up to which the times table is printed.
  *
- * Description: This function prints the times table up to the given number (n).
+ * Description: This function prints the times table to the given number (n).
  * The function ensures that the output is formatted correctly.
  */
 
@@ -17,51 +17,33 @@ void print_times_table(int n)
 	{
 		while (i <= n)
 		{
-			j = 0;
+			j = 1;
+			putchar('0');
 			while (j <= n)
 			{
+				putchar(',');
+				putchar(' ');
 				number = i * j;
-				if (j != 0)
+				if (number <= 99)
 				{
-					if (number <= 9)
-					{
-						putchar(',');
-						putchar(' ');
-						putchar(' ');
-						putchar(' ');
-					}
-					else if (number <= 99)
-					{
-						putchar(',');
-						putchar(' ');
-						putchar(' ');
-					}
-					else
-					{
-						putchar(',');
-						putchar(' ');
-					}
+					putchar(' ');
 				}
-
-				if (number < 10)
+				if (number <= 9)
 				{
-					putchar('0' + number);
+					putchar(' ');
 				}
-				else if (number < 100)
-				{
-					putchar('0' + (number / 10));
-					putchar('0' + (number % 10));
-				}
-				else
+				if (number >= 100)
 				{
 					putchar('0' + (number / 100));
 					putchar('0' + ((number / 10) % 10));
-					putchar('0' + (number % 10));
 				}
-
+				else if (number <= 99 && number >= 10)
+				{
+					putchar('0' + (number / 10));
+				}
+				putchar('0' + (number % 10));
 				j++;
 			}
-
 			printf("\n");
 			i++;
 		}
