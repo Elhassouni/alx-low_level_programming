977a4c38110d9fbb169fbe228eddc22e4254aac1
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-01-02 23:25:30 +0100

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * print_numbers -  function that prints the numbers, from 0 to 9,
+  * followed  by a new line.
+  *
+  *  Return: return RESULT
+  *
+  */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
