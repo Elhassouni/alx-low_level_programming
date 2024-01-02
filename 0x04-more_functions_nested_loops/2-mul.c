dae81d4e2c55b5dc143cf29690b4bea3feb690b2
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-01-02 20:05:03 +0100

    2.Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _isdigit - function that checks for a digit (0 through 9)..
+  *
+  * @c:  digits and char to be check
+  *
+  * Return: return 1 if digit || return 0 otherwise
+  *
+  */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = (a * b);
+
+	return (result);
+}
