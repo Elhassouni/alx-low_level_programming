bac55934c7616b28292b0744ae39f727c3ffe34a
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-01-03 19:44:07 +0100

    9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * fizz_bazz - prints Fizz, Buzz, or FizzBuzz based on certain conditions.
+ * @num: Limit for the loop.
+ *
+ * Return: Always 0.
+ */
+void fizz_bazz(int num)
+{
+	for (int i = 1; i <= num; ++i)
+	{
+		int n, n_2;
+
+		n = i % 3;
+		n_2 = i % 5;
+		if (n == 0 && n_2 == 0)
+		{
+			printf("FizzBuzz, ");
+		}
+		else if (n == 0)
+		{
+		else if (n_2 == 0)
+		{
+			printf("Buzz, ");
+		}
+		else
+		{
+			printf("%d, ", i);
+		}
+	}
+	printf("\n");
+}
