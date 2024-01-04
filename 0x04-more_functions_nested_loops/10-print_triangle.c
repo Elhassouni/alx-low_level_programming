459251cbe3a76a247c94f9fb63c36d71548d67ac
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-01-04 14:42:36 +0100

    10. Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * print_triangle - function that prints a triangle, followed by a new line.
+ *
+ * @size: Limit for the loop.
+ *
+ * Return: Always 0.
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j, k;
+
+		i = 0;
+		while (i <= size)
+		{
+			j = 0;
+			while (j < size - 1)
+			{
+
+				_putchar(' ');
+				j++;
+			}
+			k = 0;
+			while (k < i)
+			{
+				_putchar('#');
+				k++;
+			}
+		_putchar('\n');
+		i++;
+		}
+	}
+}
