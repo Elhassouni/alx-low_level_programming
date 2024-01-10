9a1c1324e789a03e5744b0d525f805a5ea563e19
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-01-11 00:31:43 +0100

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - Write a function that prints every other character of a string,
+ * starting with the first character, followed by a new line.
+ *
+ * @str: pointer to the first address on the string
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i, length;
+
+	length = strlen(str) - 1;
+	for (i = 0; i < length; i++)
+	{
+		_putchar(*(str + i));
+	}
+}
