b62fa85948c5f0c0c415849c725061e22380a8b1
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-01-18 23:05:45 +0100

    pointersArray

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,23 +9,22 @@
 
 int _atoi(char *s)
 {
-	int res, i, sign, digitFound;
+	int i, digitFound, sign;
+	int res;
 
 	sign = 1;
 	i = 0;
-	res = 0;
 	digitFound = 0;  /* Flag to track if at least one digit is found */
 
 	while ((s[i] >= 9 && s[i] <= 13) || s[i] == 32)
 	{
 		i++;
 	}
-	while (s[i] == '+' || s[i] == '-')
+	while (s[i] == '-')
 	{
 		sign = (s[i] == '-') ? -1 : 1;
 		i++;
 	}
-
 	for (; s[i] != '\0'; i++)
 	{
 		if (s[i] >= '0' && s[i] <= '9')
