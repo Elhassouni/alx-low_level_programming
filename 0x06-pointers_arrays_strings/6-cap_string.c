239ffcc4c956438569bef6714d114a8409b7a613
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-01-24 17:44:07 +0100

    cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,22 +8,23 @@
  */
 char *cap_string(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    while (s[i] != '\0')
-    {
-        if ((i == 0 || s[i - 1] == '\t' ||
-             s[i - 1] == '\n' || s[i - 1] == ' ' ||
-             s[i - 1] == '!' || s[i - 1] == '"' ||
-             s[i - 1] == '(' || s[i - 1] == ')' ||
-             s[i - 1] == '.' || s[i - 1] == ';' ||
-             s[i - 1] == '?' || s[i - 1] == '{' ||
-             s[i - 1] == '}') && (s[i] >= 'a' && s[i] <= 'z'))
-        {
-            s[i] -= 32; 
-        }
-        i++;
-    }
+	while (s[i] != '\0')
+	{
+		if ((i == 0 || s[i - 1] == 9 /* ASCII for '\t' */ ||
+s[i - 1] == 10 /* ASCII for '\n' */ || s[i - 1] == 32 /* ASCII for ' ' */ ||
+s[i - 1] == 33 /* ASCII for '!' */ || s[i - 1] == 34 /* ASCII for '"' */ ||
+s[i - 1] == 40 /* ASCII for '(' */ || s[i - 1] == 41 /* ASCII for ')' */ ||
+s[i - 1] == 46 /* ASCII for '.' */ || s[i - 1] == 59 /* ASCII for ';' */ ||
+s[i - 1] == 63 /* ASCII for '?' */ || s[i - 1] == 123 /* ASCII for '{' */ ||
+s[i - 1] == 125 /* ASCII for '}' */) && (s[i] >= 'a' && s[i] <= 'z'))
+		{
+			s[i] -= 32; /* Convert to uppercase */
+		}
+		i++;
+	}
 
-    return s;
+	return (s);
 }
+
