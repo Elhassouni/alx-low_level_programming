11fac27b74b5e72045ccb173fe39bbda798be28a
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-02-02 13:38:41 +0100

    print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,13 +7,13 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i,j;
+	int i, j;
 	int primary = 0;
 	int secondary = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		for ( j = 0; j < size; j++)
+		for (j = 0; j < size; j++)
 		{
 			if (i == j)
 			{
