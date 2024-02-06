7fae49d07b72b2c8ddf3dfce324ffc46bdeb4dc0
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-02-06 13:29:30 +0100

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-/*
+/**
   * _puts_recursion - Prints a string using recursion and pointer arithmetic.
   *
   * @s: The string to be printed.
-  * 
+  *
   * This function prints each character of the string followed by a recursive
   * call to print the rest of the string until the null terminator is reached.
   *
