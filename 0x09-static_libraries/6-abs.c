e22d631f418fe434767cc7385948460b4b528636
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-02-09 14:49:24 +0100

    Create a script called create_static_lib.sh that creates a static library called liball.a from all the .c files that are in the current directory.

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * _abs -  function that computes the absolute value of an integer.
+  *
+  * @number: The character to check.
+  *
+  * Description: the function returns the absolute value of an integer.
+  *
+  * Return:  returns absolute value
+  */
+
+int _abs(int number)
+{
+
+	int r = abs(number);
+
+return (r);
+}
---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strcpy -  function that copies the string pointed to by src,
+ * including the terminating null byte (\0), to the buffer pointed to by dest.
+ *
+ * @dest: pointer to the first address on the string.
+ * @src: size of array.
+ *
+ * Return: return pointer dest.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
+	int i, length;
+
+	length = strlen(src);
+	for (i = 0; i < length; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
