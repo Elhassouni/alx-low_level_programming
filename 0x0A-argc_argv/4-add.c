99a6b8b90d4d926d91856c40e6e9456768118774
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-02-12 01:58:22 +0100

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,23 +8,23 @@
  * @argv: An array of strings containing the command-line arguments.
  *
  * Description:
- *   This program takes command-line arguments, treating them as intgers,
- *   and attempts to calculate the sum of valid integers found in the arguments.
- *   It checks each argument for non-digit characters in the first position,
- *   reporting an error if such characters are present. If no errors are found,
- *   the program prints the sum of valid integers to the standard output.
+ * This program takes command-line arguments, treating them as intgers,
+ * and attempts to calculate the sum of valid integers found in the arguments.
+ * It checks each argument for non-digit characters in the first position,
+ * reporting an error if such characters are present. If no errors are found,
+ * the program prints the sum of valid integers to the standard output.
  *
  * Return: return 0 for success and 1 otherwise.
  */
 int main(int argc, char *argv[])
 {
 	int j, i, result;
-	
+
 	i = 0;
 	j = 0;
 	if (argc < 1)
 	{
-		printf("%d", 0);	
+		printf("%d", 0);
 	}
 	while (i < argc)
 	{
