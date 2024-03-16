8f1ee7e2a81c2307f01617392c604884be2e40dc
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-03-16 03:45:53 +0000

    0.What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+  * print_name - function that prints a name using function pointer.
+  *
+  * @name: string for name.
+  * @f: function pointer that takes char * as parameter.
+  *
+  *
+  */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
