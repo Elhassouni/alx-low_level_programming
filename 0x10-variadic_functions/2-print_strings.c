2a770e9c03d02d4efd943c895b5878e57b97ca24
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-03-26 04:33:07 +0000

    2. One woman's constant is another woman's variable

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -32,25 +32,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			{	
 				printf("%s", ptr);
 			}
-			if (i < n - 1)
+			if (i < n - 1 && separator)
 			{
 				printf("%s", separator);
 			}
-		}
-	}
-	else
-	{
-		unsigned int j;
-
-		for (j = 0; j < n; j++)
-		{
-			char *ptr2 = va_arg(elem, char *);
-
-			if (ptr2 == NULL)
-			{
-				printf("nil");
-			}
-			printf("%s", ptr2);
 		}
 	}
 	va_end(elem);
