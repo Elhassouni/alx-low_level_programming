9ca1e0ffbdb33679cf0443bd5836c2c18d3d3c33
Elhassouni <ayoub.elhassouni@usmba.ac.ma>
2024-03-29 02:31:52 +0000

    3. To be is a to be the value of a variable

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,111 @@
+#include "variadic_functions.h"
+
+/**
+ * _char - print a char
+ *
+ * @arg: a list of argument pointing
+ *      to the character to be printed
+ *
+ * Return: nothing
+*/
+
+void _char(va_list arg)
+{
+	char c = va_arg(arg, int);
+
+	printf("%c", c);
+}
+
+/**
+ * _int - print an integer
+ *
+ * @arg: a list of argument pointing
+ *      to the character to be printed
+ *
+ * Return: nothing
+*/
+
+void _int(va_list arg)
+{
+	int n = va_arg(arg, int);
+
+	printf("%d", n);
+}
+
+/**
+ * _float - print a float
+ *
+ * @arg: a list of argument pointing
+ *      to the character to be printed
+ *
+ * Return: nothing
+*/
+
+void _float(va_list arg)
+{
+	float n = va_arg(arg, double);
+
+	printf("%f", n);
+}
+
+/**
+ * _string - print a string
+ *
+ * @arg: a list of argument pointing
+ *      to the character to be printed
+ *
+ * Return: nothing
+*/
+
+void _string(va_list arg)
+{
+	char *str = va_arg(arg, char *);
+
+	if (!str)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", str);
+}
+
+/**
+ * print_all - a function that prints anything
+ *
+ * @format: A string of character representing
+ *          the argument types
+ *
+ * Description: If any argument not of type char,
+ *              int, float or char * is ignored
+ *
+ * Return: nothing
+*/
+void print_all(const char * const format, ...)
+{
+	unsigned int i;
+	const char *ptr;
+	unsigned int len;
+	va_list ap;
+	char *separator = "";
+
+	va_start(ap, format);
+	symbols all_functions[] = {
+		{'i', _int},
+		{'f', _float},
+		{'c', _char},
+		{'s', _string}
+	};
+	while (format && format[i])
+	{
+		j = 0;
+		while (j < 4 && (format[i] != all_functions[j].c)
+			j++;
+		if (j < 4)
+		{
+			printf("%s", separator);
+			all_functions[j].func(ap);
+			separator = ", ";
+		}
+		i++;
+	}
+}
